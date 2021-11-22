f61cd279d16035e2479556229c3c8f7e9cce0bdb
vakhlovav <vakhlov.av@phystech.edu>
2021-11-22 16:32:52 +0300

    changed 2D

--- D.cpp
+++ D.cpp
@@ -1,88 +1,87 @@
 #include <iostream>
 #include <cstdio>
 #include <cstring>
-
+ 
 class Heap {
 
-        int* pointer;
-        int* num;
-        long long* heap;
-        int heapSize;
-        int pointerSize;
-        int heap_bufferSize;
-        int pointer_bufferSize;
-
-    public:
-
-        Heap() {
-            heap_bufferSize = 32;
-            pointer_bufferSize = 32;
-            heapSize = 0;
-            pointerSize = 0;
-            pointer = new int [pointer_bufferSize];
-            num = new int [heap_bufferSize];
-            heap = new long long [heap_bufferSize];
-        }
-
-        Heap(const Heap& other) {
-            *this = other;
-        }
-
-        Heap& operator=(const Heap& other) {
-            if (this == &other) {
-                return *this;
-            }
-            heap_bufferSize = other.heap_bufferSize;
-            pointer_bufferSize = other.pointer_bufferSize;
-            heapSize = other.heapSize;
-            pointerSize = other.pointerSize;
-            pointer = new int [pointer_bufferSize];
-            num = new int [heap_bufferSize];
-            heap = new long long [heap_bufferSize];
-            memcpy(pointer, other.pointer, pointerSize);
-            memcpy(heap, other.heap, heapSize);
-            memcpy(num, other.num, heapSize);
-        }
-
-        ~Heap () {
-            delete [] heap;
-            delete [] pointer;
-            delete [] num;
-        }
-
-        void exchange(int u, int v) {
-            std::swap(pointer[num[u]], pointer[num[v]]);
-            std::swap(num[u], num[v]);
-            std::swap(heap[u], heap[v]);
-        }
-        
-        void siftUp(int v) {
-            while (v > 1) {
-                if (heap[v] < heap[v / 2]) {
-                    
-                    exchange(v, v / 2);
-                    v /= 2;
-                }
-                else break;
-            }
-        }
-
-        void siftDown(int v) {
-            while (2 * v <= heapSize) {
-                int u = 2 * v;
+    int heapSize = 0;
+    int pointerSize = 0;
+    int heap_bufferSize = 2;
+    int pointer_bufferSize = 2; 
+    int* pointer = new int [pointer_bufferSize];
+    int* num = new int [heap_bufferSize];
+    long long* heap = new long long [heap_bufferSize];
  
-                if (2 * v + 1 <= heapSize && heap[2 * v + 1] < heap[2 * v]) {
-                    u = 2 * v + 1;
-                }
-                if (heap[u] < heap[v]) {
+    void exchange(int u, int v) {
+        std::swap(pointer[num[u]], pointer[num[v]]);
+        std::swap(num[u], num[v]);
+        std::swap(heap[u], heap[v]);
+    }
  
-                    exchange(u, v); 
-                    v = u;
-                }
-                else break;
-            }
+public:
+ 
+    Heap() = default;
+     
+    Heap(const Heap& other) : heap_bufferSize(other.heap_bufferSize),
+            pointer_bufferSize(other.pointer_bufferSize), 
+            heapSize(other.heapSize), pointerSize(other.pointerSize),
+            pointer(new int [pointer_bufferSize]),
+            num(new int [heap_bufferSize]),
+            heap(new long long [heap_bufferSize]) {
+        memcpy(pointer, other.pointer, pointerSize);
+        memcpy(heap, other.heap, heapSize);
+        memcpy(num, other.num, heapSize);
+    }
+ 
+    Heap& operator=(const Heap& other) {
+        if (this == &other) {
+            return *this;
         }
-
+        delete [] heap;
+        delete [] pointer;
+        delete [] num;
+        heap_bufferSize = other.heap_bufferSize;
+        pointer_bufferSize = other.pointer_bufferSize;
+        heapSize = other.heapSize;
+        pointerSize = other.pointerSize;
+        pointer = new int [pointer_bufferSize];
+        num = new int [heap_bufferSize];
+        heap = new long long [heap_bufferSize];
+        memcpy(pointer, other.pointer, pointerSize);
+        memcpy(heap, other.heap, heapSize);
+        memcpy(num, other.num, heapSize);
+    }
+ 
+    ~Heap () {
+        delete [] heap;
+        delete [] pointer;
+        delete [] num;
+    }
+    
+    void siftUp(int v) {
+        while (v > 1) {
+            if (heap[v] < heap[v / 2]) {
+                exchange(v, v / 2);
+                v /= 2;
+            }
+            else break;
+        }
+    }
+ 
+    void siftDown(int v) {
+        while (2 * v <= heapSize) {
+            int u = 2 * v;
+            if (u + 1 <= heapSize && heap[u + 1] < heap[u]) {
+                ++u;
+            }
+            if (heap[u] < heap[v]) {
+                exchange(u, v);
+                v = u;
+            }
+            else break;
+        }
+    }
+ 
         void checkSize() {
             if (heapSize >= heap_bufferSize - 1) {
                 updateHeapSize();
@@ -91,7 +90,7 @@ class Heap {
                 updatePointerSize();
             }
         }
-
+ 
         void updateHeapSize() {
             heap_bufferSize = std::min(heap_bufferSize * 2, 1000000);
             long long* heapCopy = new long long [heap_bufferSize];
@@ -103,7 +102,7 @@ class Heap {
             heap = heapCopy;
             num = numCopy;
         }
-
+ 
         void updatePointerSize() {
             pointer_bufferSize = std::min(pointer_bufferSize * 2, 1000000);
             int* pointerCopy = new int [pointer_bufferSize];
@@ -112,55 +111,55 @@ class Heap {
             pointer = pointerCopy;
         }
 
-        void insert(long long x) {
-            checkSize();
-            ++heapSize;
-            ++pointerSize;
-            num[heapSize] = pointerSize;
-            heap[heapSize] = x;
-            pointer[pointerSize] = heapSize;
-            siftUp(heapSize);
-        }
-
-        long long getMin() {
-            checkSize();
-            ++pointerSize;
-            return heap[1];
-        }
-
-        void extractMin() {
-            checkSize();
-            ++pointerSize;
-            exchange(1, heapSize);
-            --heapSize;
-            siftDown(1);
-        }
-
-        void decreaseKey(int v, int delta) {
-            checkSize();
-            ++pointerSize;
-            heap[pointer[v]] -= delta;
-            siftUp(pointer[v]);
-            siftDown(pointer[v]);
-        }
+    void insert(int x) {
+        checkSize();
+        ++heapSize;
+        ++pointerSize;
+        num[heapSize] = pointerSize;
+        heap[heapSize] = x;
+        pointer[pointerSize] = heapSize;
+        siftUp(heapSize);
+    }
+ 
+    long long getMin() {
+        checkSize();
+        ++pointerSize;
+        return heap[1];
+    }
+ 
+    void extractMin() {
+        checkSize();
+        ++pointerSize;
+        exchange(1, heapSize);
+        --heapSize;
+        siftDown(1);
+    }
+ 
+    void decreaseKey(int v, int delta) {
+        checkSize();
+        ++pointerSize;
+        heap[pointer[v]] -= delta;
+        siftUp(pointer[v]);
+        siftDown(pointer[v]);
+    }
 };
-
+ 
 int main() {
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr);std::cout.tie(nullptr);
-
+ 
     int q = 0;
     std::cin >> q;
-
+ 
     Heap h;
-
+ 
     for (int i = 1; i <= q; ++i) {
-
+ 
         std::string cmd;
         std::cin >> cmd;
-
+ 
         if (cmd == "insert") {
-            long long x;
+            int x;
             std::cin >> x;
             h.insert(x);
         }
