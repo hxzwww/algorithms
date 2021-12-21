ec6f7866d607342998d58e47178e33ae0173b130
vakhlovav <vakhlov.av@phystech.edu>
2021-12-21 23:26:59 +0300

    2H

--- H.cpp
+++ H.cpp
@@ -4,22 +4,16 @@
 const int len = 256;
  
 unsigned long long bytePart(int part, unsigned long long num) {
-    
     return (num >> (8 * (part - 1))) % len;
 }
  
 unsigned long long* lsdSort(unsigned long long* array, int size) {
- 
     unsigned long long* answer = new unsigned long long [size + 1];
- 
     for (int i = 1; i <= 8; i++) {
- 
         int* cnt = new int [len];
- 
         for (int k = 0; k < len; k++) {
             cnt[k] = 0;
         }
- 
         for (int k = 0; k < size; k++) {
             cnt[bytePart(i, array[k])]++;
         }
@@ -33,10 +27,9 @@ unsigned long long* lsdSort(unsigned long long* array, int size) {
         for (int k = 1; k <= size; k++) {
             array[k - 1] = answer[k];
         }
- 
         delete [] cnt;
     }
- 
+    delete [] array; 
     return answer;
 }
  
@@ -45,18 +38,13 @@ int main() {
  
     int size = 0;
     std::cin >> size;
- 
     unsigned long long* array = new unsigned long long [size];
- 
     for (int i = 0; i < size; ++i) {
         scanf("%llu", &array[i]);
     }
- 
     array = lsdSort(array, size);
- 
     for (int i = 1; i <= size; i++) {
         printf("%llu ", array[i]);
     }
- 
     delete [] array;
 }
